aa5cc1e5a0aa541c79b6c9c9c41fea6a12dd50c5
EliusCaleb <eliuswanyoro@gmail.com>
2022-10-28 08:31:24 +0300

    Learning

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,19 +6,23 @@ class Triangle
     @side1 = side1
     @side2 = side2
     @side3  = side3
-    
-    if side1 >= (side2 + side3) || side2 >= (side1 + side3) || side3 >= (side1 + side2)
-      raise TriangleError
-    end
+
     if side1 <= 0 || side2 <= 0 || side3 <= 0
       raise TriangleError
     end
-    if side1 == nil || side2 == nil || side3 == nil
+
+    if side1 >= (side2 + side3) || side2 >= (side1 + side3) || side3 >= (side1 + side2)
       raise TriangleError
     end
 
+    
+
+    
   end
 
+
+    
+    
   
   def kind
     if ((@side1 == @side2) && (@side1 == @side3) && (@side2 == @side3))
@@ -28,14 +32,14 @@ class Triangle
     
     else 
       return :scalene
-    
     end
-  end 
+
+   
+  end
+
   
   class TriangleError < StandardError
-    def message
-      "This is not a valid triangle."
-    end
+    
   end
 
 
